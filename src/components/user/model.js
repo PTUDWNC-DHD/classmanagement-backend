4ea9dfff55773e94ebc1d8a442661b8d4d1ccc21
Nguyen Ngoc Duc <55783340+nnduc2207@users.noreply.github.com>
2021-11-13 21:21:01 +0700

    Create User Schema

---
+++ src/components/user/model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (value) {
+                const re =
+                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                return re.test(value.toLowerCase())
+            },
+            message: "Invalid email",
+        },
+    },
+    code: {
+        type: String,
+        unique: true,
+        uppercase: true,
+    },
+})
+
+const User = mongoose.model("users", UserSchema)
+
+module.exports = User
