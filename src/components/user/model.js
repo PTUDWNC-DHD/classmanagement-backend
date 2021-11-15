f483df35ca5aaadf57535feebf4ccd79412ecd9b
Nguyen Ngoc Duc <55783340+nnduc2207@users.noreply.github.com>
2021-11-15 21:39:53 +0700

    Setup validator for User

--- src/components/user/model.js
+++ src/components/user/model.js
@@ -3,14 +3,12 @@ const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        unique: true,
     },
     password: {
         type: String,
     },
     email: {
         type: String,
-        unique: true,
         validate: {
             validator: function (value) {
                 const re =
@@ -22,14 +20,68 @@ const UserSchema = new mongoose.Schema({
     },
     code: {
         type: String,
-        required: true,
-        unique: true,
-        uppercase: true,
     },
     name: {
         type: String,
         required: true,
     }
+})
+
+UserSchema.pre('save', async function(next) {
+    if (!this.code && !this.email) {
+        throw 'User must have code or email'
+    }
+    if (this.email && !(this.username && this.password)) {
+        throw 'User must have both username and password'
+    }
+
+    if (this.code) {
+        const user = await User.findOne({ code: this.code })
+        if (user) {
+            throw 'Student code has been used'
+        }
+    }
+
+    if (this.email) {
+        const user = await User.findOne({ email: this.email })
+        if (user) {
+            throw 'Email has been used'
+        }
+    }
+
+    if (this.username) {
+        const user = await User.findOne({ username: this.username })
+        if (user) {
+            throw 'Username has been used'
+        }
+    }
+
+    next()
+})
+
+UserSchema.pre('findOneAndUpdate', async function(next) {
+    const updateData = this._update
+    if (updateData.code) {
+        const user = await User.findOne({ code: updateData.code })
+        if (user) {
+            throw 'Student code has been used'
+        }
+    }
+
+    if (updateData.email) {
+        const user = await User.findOne({ email: updateData.email })
+        if (user) {
+            throw 'Email has been used'
+        }
+    }
+
+    if (updateData.username) {
+        const user = await User.findOne({ username: updateData.username })
+        if (user) {
+            throw 'Username has been used'
+        }
+    }
+    next()
 })
 
 const User = mongoose.model("users", UserSchema)
