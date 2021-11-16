e836b43c923389fd46786ff5441977d16cc483ab
Nguyen Ngoc Duc <55783340+nnduc2207@users.noreply.github.com>
2021-11-16 16:33:50 +0700

    Setup routers for Class, User and Participation

---
+++ src/routes/class.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const { CreateClass, GetClass } = require("../components/class/controller")
+const Class = require("../components/class/model")
+
+const router = new express.Router()
+
+router.get("/", async (req, res) => {
+    try {
+        const classes = await Class.find()
+        res.json(classes)
+    } catch (error) {
+        res.json({
+            errors: [error.toString()],
+        })
+    }
+})
+
+router.post("/", async (req, res) => {
+    const { name, ownerId } = req.body
+    try {
+        const classroom = await CreateClass({ name, ownerId })
+        res.json(classroom)
+    } catch (error) {
+        res.json({
+            errors: [error.toString()],
+        })
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const classroom = await GetClass(id)
+        res.json(classroom)
+    } catch (error) {
+        res.json({
+            errors: [error.toString()],
+        })
+    }
+})
+
+module.exports = router
---
+++ src/routes/participation.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const { CreateParticipation, DeleteParticipation } = require('../components/participation/controller')
+const Participation = require('../components/participation/model')
+
+const router = new express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const participations = await Participation.find()
+        return res.json(participations)
+    } catch (error) {
+        res.json({
+            errors: [
+                error.toString(),
+            ]
+        })
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const { userId, classId, isStudent } = req.body
+        const participation = await CreateParticipation({ userId, classId, isStudent })
+        return res.json(participation)
+    } catch (error) {
+        res.json({
+            errors: [
+                error.toString(),
+            ]
+        })
+    }
+})
+
+router.delete('/', async (req, res) => {
+    try {
+        const { userId, classId } = req.body
+        const result = await DeleteParticipation({ userId, classId })
+        return res.json(result)
+    } catch (error) {
+        res.json({
+            errors: [
+                error.toString(),
+            ]
+        })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const participation = await Participation.findById(id)
+        return res.json(participation)
+    } catch (error) {
+        res.json({
+            errors: [
+                error.toString(),
+            ]
+        })
+    }
+})
+
+module.exports = router
